c342ce0b5877c47b89b085711c39cb04ccaae2ec
Yannis300307 <yannis300307@gmail.com>
2024-10-05 17:40:35 +0200

    Add comments

--- src/main.c
+++ src/main.c
@@ -2,11 +2,13 @@
 #include <string.h>
 #include <limits.h>
 
+// Map settings
 #define WIDTH 160
 #define HEIGHT 120
 
 #define PIXEL_SIZE 2
 
+// BitSet tools
 #define BITMASK(b) (1 << ((b) % CHAR_BIT))
 #define BITSLOT(b) ((b) / CHAR_BIT)
 #define BITSET(a, b) ((a)[BITSLOT(b)] |= BITMASK(b))
@@ -16,16 +18,19 @@
 
 #define BYTES_NEEDED BITNSLOTS(WIDTH *HEIGHT)
 
+// Draw a pixel with the given color at the given coordinates
 #define draw_pix(cx, cy, color) eadk_display_push_rect_uniform((eadk_rect_t){cx * PIXEL_SIZE, cy * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE}, color);
 
-
+// Numworks apps specific headers
 const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "Game Of Life";
 const uint32_t eadk_api_level __attribute__((section(".rodata.eadk_api_level"))) = 0;
 
+// Map and counters
 char map_bitset[BYTES_NEEDED];
 char counters[WIDTH * HEIGHT];
 char counters2[WIDTH * HEIGHT];
 
+// Function to increment counters
 #define set_at(cx, cy)                                                           \
   for (int xx = 0; xx < 3; xx++)                                                 \
   {                                                                              \
@@ -38,6 +43,7 @@ char counters2[WIDTH * HEIGHT];
     }                                                                            \
   }
 
+// Function to decrement counters
 #define clear_at(cx, cy)                                                         \
   for (int xx = 0; xx < 3; xx++)                                                 \
   {                                                                              \
@@ -50,8 +56,10 @@ char counters2[WIDTH * HEIGHT];
     }                                                                            \
   }
 
+
 int main(int argc, char *argv[])
 {
+  // Clear the screen and the map
   for (uint16_t x = 0; x < WIDTH; x++)
   {
     for (uint16_t y = 0; y < HEIGHT; y++)
@@ -62,6 +70,7 @@ int main(int argc, char *argv[])
     }
   }
 
+  // Randomize the cells
   for (uint16_t x = 0; x < WIDTH; x++)
   {
     for (uint16_t y = 0; y < HEIGHT; y++)
@@ -75,8 +84,10 @@ int main(int argc, char *argv[])
     }
   }
 
+  // Main loop
   while (true)
   {
+    // We need another array to avoid interferences between generations
     memcpy(counters2, counters, WIDTH * HEIGHT);
 
     for (uint16_t x = 0; x < WIDTH; x++)
@@ -85,7 +96,7 @@ int main(int argc, char *argv[])
       {
         int count = counters2[x + y * WIDTH];
 
-        if ((count < 2) || (count > 3))
+        if ((count < 2) || (count > 3)) // The cell dies
         {
           if (BITTEST(map_bitset, x + y * WIDTH))
           {
@@ -94,7 +105,7 @@ int main(int argc, char *argv[])
           BITCLEAR(map_bitset, x + y * WIDTH);
           draw_pix(x, y, eadk_color_white);
         }
-        else if (count == 3)
+        else if (count == 3) // The cell become alive
         {
           if (!BITTEST(map_bitset, x + y * WIDTH))
           {
@@ -106,11 +117,12 @@ int main(int argc, char *argv[])
       }
     }
 
+    // Handle the keyboard events
     eadk_keyboard_state_t keyboardState = eadk_keyboard_scan();
 
     if (eadk_keyboard_key_down(keyboardState, eadk_key_home))
     {
-      return 0;
+      return 0; // Quit
     }
 
     // eadk_timing_msleep(20); // We want speed!
