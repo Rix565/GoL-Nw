b5e745b784615e10f2f6c04e073f9bacc6b3e675
Yannis300307 <yannis300307@gmail.com>
2024-10-05 17:23:45 +0200

    Optimised using counters

--- src/main.c
+++ src/main.c
@@ -14,70 +14,103 @@
 #define BITTEST(a, b) ((a)[BITSLOT(b)] & BITMASK(b))
 #define BITNSLOTS(nb) ((nb + CHAR_BIT - 1) / CHAR_BIT)
 
-#define BYTES_NEEDED BITNSLOTS(WIDTH*HEIGHT)
-
+#define BYTES_NEEDED BITNSLOTS(WIDTH *HEIGHT)
 
 const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "Game Of Life";
-const uint32_t eadk_api_level  __attribute__((section(".rodata.eadk_api_level"))) = 0;
+const uint32_t eadk_api_level __attribute__((section(".rodata.eadk_api_level"))) = 0;
 
-char front[BYTES_NEEDED];
-char back[BYTES_NEEDED];
+char map_bitset[BYTES_NEEDED];
+char counters[WIDTH * HEIGHT];
+char counters2[WIDTH * HEIGHT];
 
-
-int main(int argc, char * argv[]) {
-  for (int x = 0; x < WIDTH; x++) {
-      for (int y = 0; y < HEIGHT; y++) {
-        BITCLEAR(front, x+y*WIDTH);
-        eadk_display_push_rect_uniform((eadk_rect_t){(uint16_t)(x*PIXEL_SIZE), (uint16_t)(y*PIXEL_SIZE), (uint16_t)(PIXEL_SIZE), (uint16_t)(PIXEL_SIZE)}, eadk_color_white);
-      }
+#define set_at(cx, cy)                                                           \
+  for (int xx = 0; xx < 3; xx++)                                                 \
+  {                                                                              \
+    for (int yy = 0; yy < 3; yy++)                                               \
+    {                                                                            \
+      if (!(xx == 1 && yy == 1))                                                 \
+      {                                                                          \
+        counters[(cx + xx - 1) % WIDTH + ((cy + yy - 1) % HEIGHT) * WIDTH] += 1; \
+      }                                                                          \
+    }                                                                            \
   }
 
-  for (int x = 0; x < WIDTH; x++) {
-      for (int y = 0; y < HEIGHT; y++) {
-        if (eadk_random() > 1<<31) {
-          BITSET(front, x+y*WIDTH);
-          eadk_display_push_rect_uniform((eadk_rect_t){(uint16_t)(x*PIXEL_SIZE), (uint16_t)(y*PIXEL_SIZE), (uint16_t)(PIXEL_SIZE), (uint16_t)(PIXEL_SIZE)}, eadk_color_black);
+#define clear_at(cx, cy)                                                         \
+  for (int xx = 0; xx < 3; xx++)                                                 \
+  {                                                                              \
+    for (int yy = 0; yy < 3; yy++)                                               \
+    {                                                                            \
+      if (!(xx == 1 && yy == 1))                                                 \
+      {                                                                          \
+        counters[(cx + xx - 1) % WIDTH + ((cy + yy - 1) % HEIGHT) * WIDTH] -= 1; \
+      }                                                                          \
+    }                                                                            \
+  }
+
+int main(int argc, char *argv[])
+{
+  for (int x = 0; x < WIDTH; x++)
+  {
+    for (int y = 0; y < HEIGHT; y++)
+    {
+      BITCLEAR(map_bitset, x + y * WIDTH);
+      counters[x + y * WIDTH] = 0;
+      eadk_display_push_rect_uniform((eadk_rect_t){(uint16_t)(x * PIXEL_SIZE), (uint16_t)(y * PIXEL_SIZE), (uint16_t)(PIXEL_SIZE), (uint16_t)(PIXEL_SIZE)}, eadk_color_white);
+    }
+  }
+
+  for (int x = 0; x < WIDTH; x++)
+  {
+    for (int y = 0; y < HEIGHT; y++)
+    {
+      if (eadk_random() > 1 << 31)
+      {
+        BITSET(map_bitset, x + y * WIDTH);
+        set_at(x, y);
+        eadk_display_push_rect_uniform((eadk_rect_t){(uint16_t)(x * PIXEL_SIZE), (uint16_t)(y * PIXEL_SIZE), (uint16_t)(PIXEL_SIZE), (uint16_t)(PIXEL_SIZE)}, eadk_color_black);
+      }
+    }
+  }
+
+  while (true)
+  {
+    memcpy(counters2, counters, WIDTH * HEIGHT);
+
+    for (int x = 0; x < WIDTH; x++)
+    {
+      for (int y = 0; y < HEIGHT; y++)
+      {
+        int count = counters2[x + y * WIDTH];
+
+        if ((count < 2) || (count > 3))
+        {
+          if (BITTEST(map_bitset, x + y * WIDTH))
+          {
+            clear_at(x, y);
+          };
+          BITCLEAR(map_bitset, x + y * WIDTH);
+          eadk_display_push_rect_uniform((eadk_rect_t){(uint16_t)(x * PIXEL_SIZE), (uint16_t)(y * PIXEL_SIZE), (uint16_t)(PIXEL_SIZE), (uint16_t)(PIXEL_SIZE)}, eadk_color_white);
         }
-      }
-  }
-
-  eadk_display_push_rect_uniform(eadk_screen_rect, eadk_color_white);
-  
-  while (true) {
-    memcpy(back, front, BYTES_NEEDED);
-
-    for (int x = 0; x < WIDTH; x++) {
-      for (int y = 0; y < HEIGHT; y++) {
-        int count = 0;
-
-        for (int v = 0; v < 3; v++) {
-          for (int w = 0; w < 3; w++) {
-            int xx = (x+v-1) % WIDTH;
-            int yy = (y+w-1) % HEIGHT;
-
-            if (xx==x && yy==y) continue;
-            
-            if (BITTEST(back, xx+yy*WIDTH)) count++;
+        else if (count == 3)
+        {
+          if (!BITTEST(map_bitset, x + y * WIDTH))
+          {
+            set_at(x, y);
           }
-        }
-
-        if ((count < 2) || (count > 3)) {
-          BITCLEAR(front, x+y*WIDTH);
-          eadk_display_push_rect_uniform((eadk_rect_t){(uint16_t)(x*PIXEL_SIZE), (uint16_t)(y*PIXEL_SIZE), (uint16_t)(PIXEL_SIZE), (uint16_t)(PIXEL_SIZE)}, eadk_color_white);
-        } else if (count == 3) {
-          BITSET(front, x+y*WIDTH);
-          eadk_display_push_rect_uniform((eadk_rect_t){(uint16_t)(x*PIXEL_SIZE), (uint16_t)(y*PIXEL_SIZE), (uint16_t)(PIXEL_SIZE), (uint16_t)(PIXEL_SIZE)}, eadk_color_black);
+          BITSET(map_bitset, x + y * WIDTH);
+          eadk_display_push_rect_uniform((eadk_rect_t){(uint16_t)(x * PIXEL_SIZE), (uint16_t)(y * PIXEL_SIZE), (uint16_t)(PIXEL_SIZE), (uint16_t)(PIXEL_SIZE)}, eadk_color_black);
         }
       }
     }
 
     eadk_keyboard_state_t keyboardState = eadk_keyboard_scan();
 
-    if (eadk_keyboard_key_down(keyboardState, eadk_key_home)) {
+    if (eadk_keyboard_key_down(keyboardState, eadk_key_home))
+    {
       return 0;
     }
 
-    eadk_timing_msleep(20);
+    //eadk_timing_msleep(20); // We want speed!
   }
   return 0;
 }
